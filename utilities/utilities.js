f2856881eea30a681b6a45fd0ef25ea345f5c57d
fisker <lionkay@gmail.com>
2025-03-24 22:31:44 +0800

    Fix

--- utilities/utilities.js
+++ utilities/utilities.js
@@ -1,9 +1,5 @@
-import { Bench } from "tinybench";
 import assert from "node:assert/strict";
-import styleText from "node-style-text";
-import { table } from "console-table-without-index";
-import { setFlagsFromString } from "node:v8";
-import { runInNewContext } from "node:vm";
+import { Bench } from "tinybench";
 
 async function runBench({ name, cases, run, expected }) {
   const bench = new Bench({ name: `Case: ${name}` });
@@ -22,7 +18,7 @@ async function runBench({ name, cases, run, expected }) {
   }
 
   console.log();
-  console.log(styleText.blue(bench.name));
+  console.log(bench.name);
   await bench.run();
   console.table(bench.table());
 }
