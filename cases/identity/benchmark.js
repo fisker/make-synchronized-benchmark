d12424a55b3b8c5742c72b0716d5ab9b35c2e36a
fisker <lionkay@gmail.com>
2025-03-24 22:35:30 +0800

    Fix？

--- cases/identity/benchmark.js
+++ cases/identity/benchmark.js
@@ -4,7 +4,7 @@ import makeSynchronized from "./make-synchronized.js";
 import makeSynchronous from "./make-synchronous.js";
 import synckit from "./synckit/synckit.js";
 import syncThreads from "./sync-threads/sync-threads.js";
-import awaitSync from "./await-sync.js";
+// import awaitSync from "./await-sync.js";
 import { runBench } from "../../utilities/utilities.js";
 import implementation from "./implementation.js";
 
@@ -18,7 +18,8 @@ await runBench({
     { name: "make-synchronized", fn: makeSynchronized },
     { name: "make-synchronized@0.3", fn: makeSynchronized3 },
     { name: "make-synchronous", fn: makeSynchronous },
-    { name: "await-sync", fn: awaitSync },
+    // Somehow can't run
+    // { name: "await-sync", fn: awaitSync },
     { name: "sync-threads", fn: syncThreads },
   ],
   run: ({ fn }) => fn(value),
