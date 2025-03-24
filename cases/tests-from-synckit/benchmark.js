48bf022df15bc3a70914dd7012ad7a49b5b830c1
fisker <lionkay@gmail.com>
2025-03-24 15:52:41 +0800

    Remove `deasync`

--- cases/tests-from-synckit/benchmark.js
+++ cases/tests-from-synckit/benchmark.js
@@ -1,29 +1,31 @@
-import {createRequire} from 'module'
+import { createRequire } from "module";
 import assert from "node:assert/strict";
 import fs from "node:fs/promises";
 import { runBench } from "../../utilities/utilities.js";
-import makeSynchronized from './tests/make-synchronized.js'
-import makeSynchronized03 from './tests/make-synchronized-0-3.js'
-import synckit from './tests/synckit.js'
+import makeSynchronized from "./tests/make-synchronized.js";
+import makeSynchronized03 from "./tests/make-synchronized-0-3.js";
+import synckit from "./tests/synckit.js";
 
 const filename = import.meta.filename;
 const code = await fs.readFile(filename, "utf8");
 
-const require = createRequire(import.meta.url)
+const require = createRequire(import.meta.url);
 
-const deasync = require('./tests/deasync.cjs')
-const makeSynchronizedCjs = require('./tests/make-synchronized.cjs')
-const makeSynchronized03Cjs = require('./tests/make-synchronized-0-3.cjs')
-const native = require('./tests/native.cjs')
-const syncThreads = require('./tests/sync-threads.cjs')
-const synckitCjs = require('./tests/synckit.cjs')
+// Can't install on Node.js 23
+// const deasync = require('./tests/deasync.cjs')
+const makeSynchronizedCjs = require("./tests/make-synchronized.cjs");
+const makeSynchronized03Cjs = require("./tests/make-synchronized-0-3.cjs");
+const native = require("./tests/native.cjs");
+const syncThreads = require("./tests/sync-threads.cjs");
+const synckitCjs = require("./tests/synckit.cjs");
 
 const value = Math.random();
 
 await runBench({
   name: "suit from synckit",
   cases: [
-    { name: "synckit", fn:synckit },
+    // { name: "deasync", fn:deasync },
+    { name: "synckit", fn: synckit },
     { name: "native", fn: native },
     { name: "syncThreads", fn: syncThreads },
     { name: "synckitCjs", fn: synckitCjs },
