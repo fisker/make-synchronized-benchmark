996f36e6c6986a3d5ce4ea4a71c80c455fbc822f
fisker Cheung <lionkay@gmail.com>
2025-03-26 04:23:39 +0800

    Add a note

--- cases/tests-from-synckit/benchmark.js
+++ cases/tests-from-synckit/benchmark.js
@@ -21,6 +21,8 @@ const synckitCjs = require("./tests/synckit.cjs");
 
 const value = Math.random();
 
+console.warn("⚠️ The result is unreliable")
+
 await runBench({
   name: "suit from synckit",
   cases: [
